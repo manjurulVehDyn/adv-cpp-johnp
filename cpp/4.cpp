ab071fbacb5b791e7dc579b341accd7afe749f5a
Islam Mohammad Manjurul <manjurul.islam@volvo.com>
2020-09-13 00:10:52 +0200

    done for tonight

--- cpp/4.cpp
+++ cpp/4.cpp
@@ -1,17 +1,26 @@
 // Standard exception.
+#include <Windows.h>
+
 #include <iostream>
 using namespace std;
 
 class CanGoWrong {
  public:
   CanGoWrong() {
-      char *pMemory = new char[99999999999999999];
-      delete pMemory;
+    char *pMemory = new char[99999999999999999];
+    delete[] pMemory;
   }
 };
 
 int main() {
-  CanGoWrong();
-
+  HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+  try {
+    CanGoWrong wrong;
+  } catch (bad_alloc &e) {
+    SetConsoleTextAttribute(hConsole, 6);
+    cout << "Caught exception: " << e.what() << endl;
+  }
+  SetConsoleTextAttribute(hConsole, 7);
+  cout << "Still running" << endl;
   return 0;
 }
